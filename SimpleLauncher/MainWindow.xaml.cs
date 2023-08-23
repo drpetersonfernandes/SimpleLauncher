94e4c05c556203f3d48bd1e7ccf92349c6f76e6a
Peterson <Peterson@CONSULTORIO2>
2023-08-23 18:49:28 -0300

    Add project files.

---
+++ SimpleLauncher/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace SimpleLauncher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadZipFiles();
+        }
+
+        private void LoadZipFiles()
+        {
+            try
+            {
+                // Get the directory where the application is running
+                string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                // Get all the zip files from the current directory
+                List<string> zipFiles = Directory.GetFiles(currentDirectory, "*.zip").ToList();
+
+                // Sort the list in alphabetical order
+                zipFiles.Sort();
+
+                // Extract only file names from the full path and update the ListBox
+                zipFileList.ItemsSource = zipFiles.Select(filePath => Path.GetFileNameWithoutExtension(filePath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
