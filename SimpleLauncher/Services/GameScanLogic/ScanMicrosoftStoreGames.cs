3a2c31a2429a096712108df854b5806499ae4986
Peterson Fernandes <doutorpeterson@gmail.com>
2025-12-29 00:59:16 -0300

    Expand Microsoft Store blacklist to include additional pre-installed app entries.

--- SimpleLauncher/Services/GameScanLogic/ScanMicrosoftStoreGames.cs
+++ SimpleLauncher/Services/GameScanLogic/ScanMicrosoftStoreGames.cs
@@ -28,7 +28,7 @@ public static class ScanMicrosoftStoreGames
         "Microsoft.Windows.ContentDeliveryManager", "Microsoft.Windows.ShellExperienceHost",
         "Microsoft.AsyncTextService", "Microsoft.ECApp", "Microsoft.LockApp", "Microsoft.CredDialogHost", "NVIDIA Control Panel",
         "Microsoft.WindowsSecurity", "Windows Security", "AMD Software", "Microsoft.Copilot", "Copilot",
-        "Microsoft 365 Copilot", "AMD Link", "Bloco de notas", "Microsoft Teams", "Segurança do Windows", "Realtek Audio Console"
+        "Microsoft 365 Copilot", "AMD Link", "Bloco de notas", "Microsoft Teams", "Segurança do Windows", "do Windows", "Realtek Audio Console"
     };
 
     public static async Task ScanMicrosoftStoreGamesAsync(ILogErrors logErrors, string windowsRomsPath, string windowsImagesPath)
