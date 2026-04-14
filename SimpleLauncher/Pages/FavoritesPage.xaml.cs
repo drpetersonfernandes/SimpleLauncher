1212ab5e710450a1fd90be318ea433f0eaf1dec3
drpetersonfernandes <doutorpeterson@gmail.com>
2026-04-14 15:37:07 -0300

    refactor(usings and accessibility): remove unused imports and add AutomationProperties.Name to buttons

--- SimpleLauncher/Pages/FavoritesPage.xaml.cs
+++ SimpleLauncher/Pages/FavoritesPage.xaml.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
